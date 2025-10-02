e77eb34b883ab8f06b773e534cbb45d78dcb03e6
Marvin Bentinganan <marvbentinganan@gmail.com>
2025-10-02 13:29:44 +0800

    config: update PM2 settings to use env files and single-instance fork mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,18 +9,20 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: 3001            // must match your Nginx proxy
-      }
+      },
+      env_file: "./api/.env" 
     },
     {
       name: "gloo-x-sermonshots-app",
       cwd: "./app",
       script: "build/index.js",
-      instances: "2",       // or 1 for single instance
-      exec_mode: "cluster",   // or "fork" if you prefer
+      instances: "1",       // or 1 for single instance
+      exec_mode: "fork",   // or "fork" if you prefer
       env: {
         NODE_ENV: "production",
         PORT: 5174            // must match your Nginx proxy
-      }
+      },
+      env_file: "./app/.env" 
     }
   ]
 };
